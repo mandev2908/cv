b4504ed1a3a1297039383c131acbda216b7f210a
Huynh Minh Man <huynhman2908@gmail.com>
2024-09-24 13:28:41 +0700

    update code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2a66ae8c125c869b"; }
+/******/ 		__webpack_require__.h = function() { return "9fc5cde637e1f4a1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.b66a5d424c9653f9.hot-update.js
+++ .next/static/webpack/webpack.b66a5d424c9653f9.hot-update.js
@@ -63,8 +63,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e8451d94ef35c5e8"; }
+/******/ 	__webpack_require__.h = function() { return "9fc5cde637e1f4a1"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ /* webpack/runtime/jsonp chunk loading */
 /******/ !function() {
